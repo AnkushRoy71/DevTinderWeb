591e9aeb88a7fdb8109a33a00d8ed6e2471508c0
AnkushRoy71 <royankush832@gmail.com>
2026-09-04 01:14:07 +0530

    feat: simplify header component by removing unused menu items and integrating user greeting

--- src/app/core/shared/components/header/header.ts
+++ src/app/core/shared/components/header/header.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { MenuItem } from 'primeng/api';
 import { AvatarModule } from 'primeng/avatar';
 import { BadgeModule } from 'primeng/badge';
 import { InputTextModule } from 'primeng/inputtext';
 import { MenubarModule } from 'primeng/menubar';
 import { RippleModule } from 'primeng/ripple';
+import { selectUser } from '../../state/user/user.selector';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
-  imports: [AvatarModule, BadgeModule, MenubarModule, InputTextModule, RippleModule],
+  imports: [AvatarModule, BadgeModule, MenubarModule, InputTextModule, RippleModule, AsyncPipe],
   selector: 'app-header',
   styleUrl: './header.scss',
   templateUrl: './header.html',
@@ -15,38 +18,11 @@ import { RippleModule } from 'primeng/ripple';
 })
 export class Header implements OnInit {
   items: MenuItem[] | undefined;
+  storeService = inject(Store);
+  user$ = this.storeService.select(selectUser);
+
   ngOnInit(): void {
-    this.items = [
-            {
-                label: 'Home',
-                icon: 'pi pi-home'
-            },
-            {
-                label: 'Projects',
-                icon: 'pi pi-search',
-                badge: '3',
-                items: [
-                    {
-                        label: 'Core',
-                        icon: 'pi pi-bolt',
-                        shortcut: '⌘+S'
-                    },
-                    {
-                        label: 'Blocks',
-                        icon: 'pi pi-server',
-                        shortcut: '⌘+B'
-                    },
-                    {
-                        separator: true
-                    },
-                    {
-                        label: 'UI Kit',
-                        icon: 'pi pi-pencil',
-                        shortcut: '⌘+U'
-                    }
-                ]
-            }
-        ];
+
   }
 
 }
